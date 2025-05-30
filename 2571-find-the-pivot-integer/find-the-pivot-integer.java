1e1637258654ea89875b649fd0eb388ee7b2aec5
Kannchi04 <94986556+Kannchi04@users.noreply.github.com>
2025-05-30 16:56:25 +0530

    Time: 1 ms (72.47%) | Memory: 41.1 MB (28.93%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,19 +1,42 @@
 class Solution {
     public int pivotInteger(int n) {
         
-        for(int i = 1; i<=n ; i++){
-            int leftSum = 0;
-            int rightSum = 0;
+        // for(int i = 1; i<=n ; i++){
+        //     int leftSum = 0;
+        //     int rightSum = 0;
 
-            for(int j = 1; j<=i;j++){
-                leftSum += j;
-            }
-            for(int k = i; k<=n; k++){
-                rightSum += k;
-            }
+        //     for(int j = 1; j<=i;j++){
+        //         leftSum += j;
+        //     }
+        //     for(int k = i; k<=n; k++){
+        //         rightSum += k;
+        //     }
 
-            if(leftSum == rightSum){
-                return i;
+        //     if(leftSum == rightSum){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        int left = 1;
+        int right = n;
+
+        if(n==1){
+            return 1;
+        }
+
+        int leftSum = left;
+        int rightSum = right;
+
+        while(right>left){
+            if(leftSum < rightSum){
+                left++;
+                leftSum+=left;
+            }else{
+                right--;
+                rightSum +=right;
+            }
+            if(rightSum == leftSum && right-1 == left+1){
+                return left+1;
             }
         }
         return -1;
