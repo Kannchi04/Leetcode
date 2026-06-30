51c338a8f019e575a7863377c75c99eafa6eed59
Kannchi04 <94986556+Kannchi04@users.noreply.github.com>
2026-06-30 22:12:57 +0530

    Time: 15 ms (81.18%) | Memory: 19.4 MB (52.95%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -6,23 +6,30 @@ class Solution:
                 dict1[s1[i]] = 1
             else:
                 dict1[s1[i]]+=1
-
+        i = 0
         j = len(s1)-1
-        i =0
-
-        while j<len(s2):
-            sub = s2[i:j+1]
-            dict2 = {}
-            for k in sub:
-                if k not in dict2:
-                    dict2[k] = 1
-                else:
-                    dict2[k] +=1
-                
-            if dict1 == dict2:
+        dict2 = {}
+        for k in s2[:len(s1)]:
+            if k not in dict2:
+                dict2[k] = 1
+            else:
+                dict2[k] +=1
+        if dict1 == dict2:
+            return True
+        left  = 0
+        for right in range(len(s1), len(s2)):
+            dict2[s2[left]]-=1
+            if dict2[s2[left]] == 0:
+                del dict2[s2[left]]
+            if s2[right] not in dict2:
+                dict2[s2[right]] = 1
+            else:
+                dict2[s2[right]]+=1
+            left+=1
+            if dict2 == dict1:
                 return True
-            j+=1
-            i+=1
+
+            
         return False
 
             
